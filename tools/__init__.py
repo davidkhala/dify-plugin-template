260fc0b0be9e9e2c9feace494c9020f60d3eabc0
David Liu <david-khala@hotmail.com>
2026-02-04 11:45:04 +0800

    fix naming conflict

--- tools/__init__.py
+++ tools/__init__.py
@@ -5,7 +5,7 @@ from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
 
-class DifyExtractorTool(Tool):
+class DifyTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         html_content:str = tool_parameters.get("html")
 
